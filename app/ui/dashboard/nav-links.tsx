5c709d3696d33c976dc33dfe467dddbc5e2116f6
suhsein <mini9075@naver.com>
2024-11-05 16:38:09 +0900

    ch.5 : navigating Link 사용

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,9 @@
+'use client'
+
 import { UserGroupIcon, HomeIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -13,19 +18,26 @@ const links = [
 ]
 
 export default function NavLinks() {
+  const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className='flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              { 'bg-sky-100 text-blue-600': pathname === link.href },
+            )}
           >
+            {/* a 태그 사용 시 navigating 에서 전체 페이지 재렌더링. Link 태그 사용 시 부분 재렌더링 */}
+            {/* 또한, 뷰포트에 Link가 들어오는 경우 방문 가능성이 높으므로 auto code-splitting과 prefetching을 하게 된다. */}
+            {/* 사용자가 링크 클릭 시 빠른 렌더링 */}
             <LinkIcon className='w-6' />
             <p className='hidden md:block'>{link.name}</p>
-          </a>
+          </Link>
         )
       })}
     </>
