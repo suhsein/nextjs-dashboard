ca7e1f6d966d825364f9208423b3d2c6b9ab194f
suhsein <mini9075@naver.com>
2024-11-05 14:49:19 +0900

    프리티어 설정

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -105,7 +105,7 @@ export async function GET() {
   return Response.json({
     message:
       'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  })
   // try {
   //   await client.sql`BEGIN`;
   //   await seedUsers();
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,17 +1,12 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline'
+import { lusitana } from '@/app/ui/fonts'
 
 const iconMap = {
   collected: BanknotesIcon,
   customers: UserGroupIcon,
   pending: ClockIcon,
   invoices: InboxIcon,
-};
+}
 
 export default async function CardWrapper() {
   return (
@@ -27,7 +22,7 @@ export default async function CardWrapper() {
         type="customers"
       /> */}
     </>
-  );
+  )
 }
 
 export function Card({
@@ -35,24 +30,23 @@ export function Card({
   value,
   type,
 }: {
-  title: string;
-  value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  title: string
+  value: number | string
+  type: 'invoices' | 'customers' | 'pending' | 'collected'
 }) {
-  const Icon = iconMap[type];
+  const Icon = iconMap[type]
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        <h3 className="ml-2 text-sm font-medium">{title}</h3>
+    <div className='rounded-xl bg-gray-50 p-2 shadow-sm'>
+      <div className='flex p-4'>
+        {Icon ? <Icon className='h-5 w-5 text-gray-700' /> : null}
+        <h3 className='ml-2 text-sm font-medium'>{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
     </div>
-  );
+  )
 }
